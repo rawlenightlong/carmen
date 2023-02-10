4fd94454f6a3c8d8125bae18d88bd2aa4d4dbc0d
Rawle Becerril <rawlesxm@gmail.com>
2023-02-09 19:16:07 -0500

    CARMEN SANDIEGO - I found Carmen Sandiego

--- answers.sql
+++ answers.sql
@@ -47,12 +47,12 @@ SELECT city.name, country.name FROM city JOIN country ON country.code = city.cou
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 
-
+SELECT city.name, country.name FROM city JOIN country on country.code = city.countrycode WHERE city.name ILIKE 'Serra%';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
-
-
+select * FROM country WHERE name = 'Brazil';
+select * FROM city WHERE id = 211;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
 
@@ -68,7 +68,7 @@ SELECT city.name, country.name FROM city JOIN country ON country.code = city.cou
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT name FROM city WHERE population = 91084;
 
 
-
--- She's in ____________________________!
+-- She's in Santa Monica, United States!
